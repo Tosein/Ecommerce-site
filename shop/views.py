0c783fd4e53ef97de44e217e7cc12195ce8a1e3c
Tosein <olayiwolaoluwatosin7@gmail.com>
2024-02-05 22:22:53 +0100

    added search functionality to search for products

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,8 @@ from .models import Products
 
 def index(request):
   product_objects = Products.objects.all()
+
+  product_name = request.GET.get('product_name')
+  if product_name !='' and product_name is not None:
+    product_objects = product_objects.filter(title__icontains=product_name)
   return render(request, 'shop/index.html', {'product_objects':product_objects}) 
