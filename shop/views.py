1c42b4833c95a30ee7a0ac015ae3e93cd0216230
Tosein <olayiwolaoluwatosin7@gmail.com>
2024-02-05 20:24:05 +0100

    fetching content from database to index view

--- shop/views.py
+++ shop/views.py
@@ -4,4 +4,4 @@ from .models import Products
 
 def index(request):
   product_objects = Products.objects.all()
-  return render(request, 'shop.index.html', {'product_objects':product_objects}) 
+  return render(request, 'shop/index.html', {'product_objects':product_objects}) 
